4f85d772e19b9da8e6c66b7d51fb52d7a7c643f1
Kennedy Ngure <kngure@fintech-group.com>
2021-08-06 17:11:26 +0300

    update on the get request with instaces of faker id

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const url = require('url');
+const querystring = require('querystring');
 const faker = require('faker');
 const morgan = require('morgan');
 const sub = require('date-fns/sub');
 const bodyParser = require("body-parser");
 const changeCase = require('change-case');
+const http = require('http');
+const {
+    ReasonPhrases,
+    StatusCodes,
+    getReasonPhrase,
+    getStatusCode,
+} = require('http-status-codes');
+
 
 const lodash = require('lodash');
 
@@ -11,6 +21,7 @@ const app = express();
 
 
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
@@ -47,6 +58,7 @@ const CONTACT_NAMES = [
     faker.name.suffix() + ' ' + faker.name.firstName() + ' ' + faker.name.middleName() + ' ' + faker.name.lastName(),
     faker.name.suffix() + ' ' + faker.name.firstName() + ' ' + faker.name.middleName() + ' ' + faker.name.lastName(),
     faker.name.suffix() + ' ' + faker.name.firstName() + ' ' + faker.name.middleName() + ' ' + faker.name.lastName(),
+    'T.O Auma Bro'
 
 ];
 const MY_CONTACT = {
@@ -645,49 +657,116 @@ const findConversationByOtherParticipantId = (participantId) => {
 };
 
 
-const findConversationByParticipantIds = (participantIds) => {
-    const conversation = CONVERSATIONS.find((_conversation) => {
-        if (_conversation.participants.length < participantIds.length) {
-            return false;
-        }
-        const _participantIds[] =[];
+// const findConversationByParticipantIds = (participantIds) => {
+//     const conversation = CONVERSATIONS.find((_conversation) => {
+//         if (_conversation.participants.length < participantIds.length) {
+//             return false;
+//         }
+//         const _participantIds
+//         [] = [];
+//
+//         _conversation.participants.forEach((_participant) => {
+//             _participantIds.push(_participant.id);
+//         });
+//
+//         return lodash.isEmpty(lodash.xor(participantIds, _participantIds));
+//     });
+//     return conversation || null;
+// };
 
-        _conversation.participants.forEach((_participant) => {
-            _participantIds.push(_participant.id);
-        });
 
-        return lodash.isEmpty(lodash.xor(participantIds, _participantIds));
+app.get('/', async function (req, res) {
+
+    // Access the provided 'page' and 'limt' query parameters
+    let page = req.query.page;
+    let limit = req.query.limit;
+
+    let articles = await Article.findAll().paginate({page: page, limit: limit}).exec();
+
+    // Return the articles to the rendering engine
+    res.render('index', {
+        articles: articles
     });
-    return conversation || null;
-};
+});
 
-
-app.get(`${app_prefix}/chat/contacts`, (req, res) => {
-    res.send(CONTACTS)
-    res.status(200)
-
-})
-
-
-app.get(`${app_prefix}/chat/search`, (req, res) => {
+app.get(`${app_prefix}/chat/contacts`, async function (req, res) {
     try {
-        const {query} = config.params;
-        let results = CONTACTS;
-        if (query) {
-            const cleanQuery = query.toLowerCase().trim();
-            results = results.filter((contact) => contact.name.toLowerCase().includes(cleanQuery));
-        }
-        return [200, {results}];
+        res.status(StatusCodes.OK)
+        res.json(CONTACTS)
+
     } catch (error) {
         console.error(error);
-        return [500, {message: 'Internal server error'}];
     }
 });
 
+// app.get(`${app_prefix}/chat/conversations`, async function (req, res) {
+//     try {
+//         res.status(StatusCodes.OK);
+//         res.json(CONVERSATIONS);
+//
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
 
-mock.onGet('/api/chat/participants').reply((config) => {
+
+app.get(`${app_prefix}/chat/conversations`, async function (req, res) {
     try {
-        const {conversationKey} = config.params;
+        var conversationKey = req.query.conversationKey;
+        let conversation = findConversationById(conversationKey);
+        res.json(conversation);
+
+        if (conversation) {
+            res.status(StatusCodes.OK);
+            res.json(conversation);
+        }
+
+        const contact = findContactByUsername(conversationKey);
+
+        if (!contact) {
+
+            res.status(StatusCodes.NOT_FOUND);
+            res.json([{message: getReasonPhrase.NOT_FOUND}]);
+        }
+        conversation = findConversationByOtherParticipantId(contact.id);
+
+        res.status(StatusCodes.OK);
+        res.json(CONVERSATIONS);
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+        res.json([{message: getReasonPhrase.INTERNAL_SERVER_ERROR}]);
+
+    }
+})
+;
+
+app.get(`${app_prefix}/chat/search`, async function (req, res) {
+    try {
+
+        const filters = req.query;
+
+        const filteredUsers = CONTACTS.filter(CONTACTS => {
+            let isValid = true;
+            for (let key in filters) {
+                console.log(key, CONTACTS[key], filters[key]);
+                isValid = isValid && CONTACTS[key] === filters[key];
+            }
+            return isValid;
+        });
+
+        res.status(StatusCodes.OK);
+        res.json(filteredUsers);
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+        res.json({message: ReasonPhrases.INTERNAL_SERVER_ERROR});
+    }
+});
+app.get(`${app_prefix}/chat/participants`, async function (req, res) {
+    try {
+
+        var conversationKey = req.query.conversationKey;
         const participants = [];
         const conversation = findConversationById(conversationKey);
         if (conversation) {
@@ -709,49 +788,29 @@ mock.onGet('/api/chat/participants').reply((config) => {
                 });
             }
         }
-        return [200, {participants}];
+
+        res.status(StatusCodes.OK);
+        res.json(participants);
     } catch (error) {
         console.error(error);
-        return [500, {message: 'Internal server error'}];
-    }
-});
-// ----------------------------------------------------------------------
-
-// ----------------------------------------------------------------------
-
-mock.onGet('/api/chat/conversation').reply((config) => {
-    try {
-        const {conversationKey} = config.params;
-        let conversation = findConversationById(conversationKey);
-
-        if (conversation) {
-            return [200, {conversation}];
-        }
-
-        const contact = findContactByUsername(conversationKey);
-
-        if (!contact) {
-            return [404, {message: 'Unable to find the contact'}];
-        }
-        conversation = findConversationByOtherParticipantId(contact.id);
-
-        return [200, {conversation}];
-    } catch (error) {
-        console.error(error);
-        return [500, {message: 'Internal server error'}];
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+        res.json(error);
     }
 });
 
-// ----------------------------------------------------------------------
+app.get(`${app_prefix}/chat/conversation/mark-as-seen`, async function (req, res) {
 
-mock.onGet('/api/chat/conversation/mark-as-seen').reply((config) => {
     try {
-        const {conversationId} = config.params;
+        const conversationId = config.params;
         const conversation = CONVERSATIONS.find((_conversation) => _conversation.id === conversationId);
         if (conversation) {
             conversation.unreadCount = 0;
         }
-        return [200, true];
+        res.status(StatusCodes.OK);
+
+        res.json(
+            {'true': true, conversation}
+        );
     } catch (error) {
         console.error(error);
         return [500, {message: 'Internal server error'}];
@@ -760,78 +819,106 @@ mock.onGet('/api/chat/conversation/mark-as-seen').reply((config) => {
 
 // ----------------------------------------------------------------------
 
-mock.onPost('/api/chat/messages/new').reply((request) => {
-    try {
-        const {
-            conversationId,
-            recipientIds,
-            body
-        } = JSON.parse(request.data);
+// mock.onGet('/api/chat/conversation').reply((config) => {
+//     try {
+//         const {conversationKey} = config.params;
+//         let conversation = findConversationById(conversationKey);
+//
+//         if (conversation) {
+//             return [200, {conversation}];
+//         }
+//
+//         const contact = findContactByUsername(conversationKey);
+//
+//         if (!contact) {
+//             return [404, {message: 'Unable to find the contact'}];
+//         }
+//         conversation = findConversationByOtherParticipantId(contact.id);
+//
+//         return [200, {conversation}];
+//     } catch (error) {
+//         console.error(error);
+//         return [500, {message: 'Internal server error'}];
+//     }
+// });
 
-        const user = MY_CONTACT;
-        let conversation = null;
+// ----------------------------------------------------------------------
 
-        if (conversationId) {
-            conversation = findConversationById(conversationId);
-            if (!conversation) {
-                return [400, {message: 'Invalid conversation id'}];
-            }
-        }
 
-        if (recipientIds) {
-            const participantIds = [...recipientIds, user.id];
-            conversation = findConversationByParticipantIds(participantIds);
-        }
+// ----------------------------------------------------------------------
 
-        const message = {
-            id: faker.datatype.uuid(),
-            attachments: [],
-            body,
-            contentType: 'text',
-            createdAt: sub(new Date(), {minutes: 1}),
-            senderId: user.id
-        };
-
-        if (conversation) {
-            conversation.messages = [...conversation.messages, message];
-        } else {
-            const participants = [user];
-
-            recipientIds.forEach((recipientId) => {
-                const contact = CONTACTS.find((_contact) => _contact.id === recipientId);
-
-                if (!contact) {
-                    throw new Error('Contact not found');
-                }
-
-                participants.push({
-                    id: contact.id,
-                    avatar: contact.avatar,
-                    name: contact.name,
-                    username: contact.username
-                });
-            });
-
-            conversation = {
-                id: faker.datatype.uuid(),
-                messages: [message],
-                participants,
-                type: participants.length === 2 ? 'ONE_TO_ONE' : 'GROUP',
-                unreadCount: 0
-            };
-        }
-
-        const responseData = {
-            conversationId: conversation.id,
-            message
-        };
-
-        return [200, responseData];
-    } catch (error) {
-        console.error(error);
-        return [500, {message: 'Internal server error'}];
-    }
-});
+// mock.onPost('/api/chat/messages/new').reply((request) => {
+//     try {
+//         const {
+//             conversationId,
+//             recipientIds,
+//             body
+//         } = JSON.parse(request.data);
+//
+//         const user = MY_CONTACT;
+//         let conversation = null;
+//
+//         if (conversationId) {
+//             conversation = findConversationById(conversationId);
+//             if (!conversation) {
+//                 return [400, {message: 'Invalid conversation id'}];
+//             }
+//         }
+//
+//         if (recipientIds) {
+//             const participantIds = [...recipientIds, user.id];
+//             conversation = findConversationByParticipantIds(participantIds);
+//         }
+//
+//         const message = {
+//             id: faker.datatype.uuid(),
+//             attachments: [],
+//             body,
+//             contentType: 'text',
+//             createdAt: sub(new Date(), {minutes: 1}),
+//             senderId: user.id
+//         };
+//
+//         if (conversation) {
+//             conversation.messages = [...conversation.messages, message];
+//         } else {
+//             const participants = [user];
+//
+//             recipientIds.forEach((recipientId) => {
+//                 const contact = CONTACTS.find((_contact) => _contact.id === recipientId);
+//
+//                 if (!contact) {
+//                     throw new Error('Contact not found');
+//                 }
+//
+//                 participants.push({
+//                     id: contact.id,
+//                     avatar: contact.avatar,
+//                     name: contact.name,
+//                     username: contact.username
+//                 });
+//             });
+//
+//             conversation = {
+//                 id: faker.datatype.uuid(),
+//                 messages: [message],
+//                 participants,
+//                 type: participants.length === 2 ? 'ONE_TO_ONE' : 'GROUP',
+//                 unreadCount: 0
+//             };
+//         }
+//
+//         const responseData = {
+//             conversationId: conversation.id,
+//             message
+//         };
+//
+//         return [200, responseData];
+//     } catch (error) {
+//         console.error(error);
+//         return [500, {message: 'Internal server error'}];
+//     }
+// });
 
 
 app.listen(app_port, () => {
