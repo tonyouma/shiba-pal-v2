e650c9c9f42bd31ca9e4eb84bc96af1bb0884180
Kennedy Ngure <kngure@fintech-group.com>
2021-08-09 11:50:21 +0300

    added login routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,12 @@ const router = express.Router();
 
 router
     .post('/login/:userId', encode, (req, res, next) => {
+        return res
+            .status(200)
+            .json({
+                success: true,
+                authorization: req.authToken,
+            });
     });
 
 export default router;
