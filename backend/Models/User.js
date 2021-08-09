e79bbe8fef016d0727d98f63e379d2c8d0211e4a
Kennedy Ngure <kngure@fintech-group.com>
2021-08-09 11:37:22 +0300

    added the controllers for delete users'

---
+++ backend/Models/User.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import {v4 as uuidv4} from "uuid";
+
+export const USER_TYPES = {
+    CONSUMER: "consumer",
+    SUPPORT: "support",
+};
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            default: () => uuidv4().replace(/\-/g, ""),
+        },
+        firstName: String,
+        lastName: String,
+        type: String,
+    },
+    {
+        timestamps: true,
+        collection: "users",
+    }
+);
+userSchema.statics.getUserById = async function (id) {
+    try {
+        const user = await this.findOne({_id: id});
+        if (!user) throw ({error: 'No user with this id found'});
+        return user;
+    } catch (error) {
+        throw error;
+    }
+
+
+}
+userSchema.statics.getUsers = async function () {
+    try {
+        return await this.find();
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+userSchema.statics.createUser = async function (
+    firstName,
+    lastName,
+    type
+) {
+    try {
+        return await this.create({firstName, lastName, type});
+    } catch (error) {
+        throw error;
+    }
+}
+userSchema.statics.deleteByUserById = async function (id) {
+    try {
+        const result = await this.remove({_id: id});
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export default mongoose.model("User", userSchema);
